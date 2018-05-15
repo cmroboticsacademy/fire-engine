09dd20b6dbd6e464567b718c8d99de896062e179
Joshua Jarvis <Joshua Jarvis>
2018-05-15 11:51:07 -0400

    User Attempts now return selcted answers in API.

--- lib/fire_engine_web/views/api/v1/answer.ex
+++ lib/fire_engine_web/views/api/v1/answer.ex
@@ -1,10 +1,15 @@
 defmodule FireEngineWeb.Api.V1.AnswerView do
   use FireEngineWeb, :view
-  def render("answer.json", %{answer: answer}) do
+  def render("answer.json", %{answer: answer, responses: responses}) do
     %{
       answer_id: answer.id,
-      answer: answer.answer
+      answer: answer.answer,
+      selected: is_selected?(answer,responses)
     }
-
   end
+
+  defp is_selected?(answer,responses) do
+    responses |> Enum.any?(&(&1.answer_id == answer.id))
+  end
+
 end
--- lib/fire_engine_web/views/api/v1/question.ex
+++ lib/fire_engine_web/views/api/v1/question.ex
@@ -1,9 +1,9 @@
 defmodule FireEngineWeb.Api.V1.QuestionView do
   use FireEngineWeb, :view
-  def render("question.json", %{question: question}) do
+  def render("question.json", %{question: question, responses: responses}) do
     %{question_id: question.id,
       question: question.content,
-      answers: render_many(question.answers, FireEngineWeb.Api.V1.AnswerView, "answer.json")
+      answers: render_many(question.answers, FireEngineWeb.Api.V1.AnswerView, "answer.json", responses: responses)
     }
   end
 end
