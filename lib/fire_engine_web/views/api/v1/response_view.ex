f3b7b761f660c1a1e9e86a72a42520da242e9375
Joshua Jarvis <Joshua Jarvis>
2018-09-24 13:50:22 -0400

    ResponseView now handles empty responses.

--- lib/fire_engine_web/views/api/v1/response_view.ex
+++ lib/fire_engine_web/views/api/v1/response_view.ex
@@ -5,7 +5,8 @@ defmodule FireEngineWeb.Api.V1.ResponseView do
 
   def render("response.json", %{response: response}) do
     %{answer_id: response.answer_id,
-      answer: get_answer(response.answer_id),
+      answer: get_answer(response.answer_id,:answer),
+      correct: get_answer(response.answer_id,:weight),
       question_id: response.question_id,
       question: Assessments.get_question!(response.question_id).content
     }
@@ -13,14 +14,17 @@ defmodule FireEngineWeb.Api.V1.ResponseView do
 
   def render("response-answer.json", %{response: response}) do
     %{answer_id: response.answer_id,
-      answer: get_answer(response.answer_id),
-      correct: Assessments.get_answer!(response.answer_id).weight,
+      answer: get_answer(response.answer_id,:answer),
+      correct: get_answer(response.answer_id,:weight),
       correct_answer: Assessments.get_correct_answer(response.question_id),
       question_id: response.question_id,
       question: Assessments.get_question!(response.question_id).content
     }
   end
 
-  defp get_answer(id) when is_nil(id), do: nil
-  defp get_answer(id) when is_integer(id), do: Assessments.get_answer!(id).answer
+  defp get_answer(id,field) when is_nil(id), do: nil
+  defp get_answer(id,field) when is_integer(id) do
+    {:ok, attribute} = Assessments.get_answer!(id) |> Map.fetch(field)
+    attribute
+  end
 end
