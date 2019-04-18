748bd0c51347e9c75dd2400331444a39a7e2f558
bynarlogic <Joshua Jarvis>
2019-04-18 12:38:43 -0400

    Updated host name to match Heroku Hostname.

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 
 config :fire_engine, FireEngineWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https",host: "evening-stream-77822", port: 443],
+  url: [scheme: "https",host: "fire-engine", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(),"SECRET_KEY_BASE_PROD"),
   cache_static_manifest: "priv/static/cache_manifest.json"
